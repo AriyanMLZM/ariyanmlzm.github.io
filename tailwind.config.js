7d8d34fe71185cb4fe97564e451eb68f773a336f
AriyanMlzm <ryanmolazem@gmail.com>
2024-05-11 19:28:17 +0330

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: '#080a1a',
+        primary: '#00bfff'
+      },
+      fontFamily: {
+        worksans: ["Work Sans", "sans-serif"],
+        poppins: ['Poppins', "sans-serif"]
+      },
+      boxShadow: {
+        card: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)'
+      }
+    },
   },
   plugins: [],
 }
+
