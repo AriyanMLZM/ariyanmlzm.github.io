e4bb23675f918dd58d47027a973dfc7d4fa954c3
AriyanMlzm <ryanmolazem@gmail.com>
2024-05-11 19:44:58 +0330

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
   assetsInclude: ['**/*.glb'],
   plugins: [react()],
 })
