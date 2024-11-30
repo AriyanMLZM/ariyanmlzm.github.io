8514a0724e328157f2abe69ea48c4871a5d7a09e
AriyanMlzm <ryanmolazem@gmail.com>
2024-11-30 20:17:17 +0330

    fix exp list

--- src/containers/Exp.tsx
+++ src/containers/Exp.tsx
@@ -6,10 +6,10 @@ const Exp = () => {
 	return (
 		<>
 			<div className="w-[25%] flex flex-col text-primary text-[20px] md:text-[25px] items-end justify-between pt-4 pb-4">
-				<h2 className="text-right">{'<p>'}</h2>
-				<h2 className="text-rignt">{'</p>'}</h2>
+				<h2 className="text-right">{'<ul>'}</h2>
+				<h2 className="text-rignt">{'</ul>'}</h2>
 			</div>
-			<div className="w-[50%] flex flex-col overflow-y-auto justify-center pl-4 text-[28px] md:text-[35px]">
+			<div className="w-[50%] flex flex-col overflow-y-auto pl-4 py-[20px] text-[28px] md:text-[35px]">
 				{about.exp.map((item: any) => (
 					<div className="mb-6" key={uuid()}>
 						<h1 className="text-white font-bold" key={uuid()}>
