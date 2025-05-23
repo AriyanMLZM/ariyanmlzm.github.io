67845ca5e7899766d0c02e95e731cfb33f626b13
AriyanMlzm <ryanmolazem@gmail.com>
2025-05-23 13:24:49 +0330

    fix vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<Analytics />
 		<App />
 	</StrictMode>
 )
